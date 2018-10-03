00eb7b872b05d10374eaa9eb54c51265cd90aae1
Shubham Vishwakarma <shubhamvishwakarma2001@gmail.com>
2018-10-03 15:50:27 +0530

    project and gradle initialisation

---
+++ app/src/main/java/com/mission/chaze/chaze/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mission.chaze.chaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
