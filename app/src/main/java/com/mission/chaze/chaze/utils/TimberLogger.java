5f3c9254f3c2442d16341fb7a50e2e1b3f0491df
Shubham Vishwakarma <shubhamvishwakarma2001@gmail.com>
2018-10-05 02:45:48 +0530

    utils

--- app/src/main/java/com/mission/chaze/chaze/utils/TimberLogger.java
+++ app/src/main/java/com/mission/chaze/chaze/utils/TimberLogger.java
@@ -2,6 +2,10 @@ package com.mission.chaze.chaze.utils;
 
 import timber.log.Timber;
 
+/**
+ * Created by Shubham Vishwakarma on 05/10/18.
+ */
+
 public class TimberLogger extends Timber.Tree {
     @Override
     protected void log(final int priority, final String tag, final String message, final Throwable throwable) {
