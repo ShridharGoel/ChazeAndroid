37b905e5f4d7f9cb64927652f0699836a59c0e69
Shubham Vishwakarma <shubhamvishwakarma2001@gmail.com>
2018-10-13 19:17:08 +0530

    Various classes related to Cart

---
+++ app/src/main/java/com/mission/chaze/chaze/models/ShopItem.java
@@ -0,0 +1,51 @@
+package com.mission.chaze.chaze.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ShopItem extends Item implements Serializable {
+
+
+    private ArrayList<String> imageResource;
+    private Shop itemShop;
+    private String category;
+
+
+    public ShopItem(String name, String productCode, String category, boolean isRecommended, ArrayList<String> imageResource, Shop itemShop, double price, boolean isVeg, boolean isFavourite, String currentRatingByUser, float rating, int ratingCount, int orderCount, double itemOffer, boolean hasOffer) {
+
+        super(name, productCode, isRecommended, price, isFavourite, currentRatingByUser, rating, ratingCount, itemOffer, hasOffer);
+        this.imageResource = imageResource;
+        this.itemShop = itemShop;
+        this.category = category;
+
+    }
+
+    public ArrayList<String> getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(ArrayList<String> imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Shop getItemShop() {
+        return itemShop;
+    }
+
+    public void setItemShop(Shop itemShop) {
+        this.itemShop = itemShop;
+    }
+
+    @Override
+    public Business getBusiness() {
+        return itemShop;
+    }
+}
