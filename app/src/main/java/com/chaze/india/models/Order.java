84bfbd474280d705c9af6ac09beeb5dddf133c6a
Shubham Vishwakarma <shubhamvishwakarma2001@gmail.com>
2018-10-19 14:10:22 +0530

    Added Orders Class

---
+++ app/src/main/java/com/chaze/india/models/Order.java
@@ -0,0 +1,32 @@
+package com.chaze.india.models;
+
+import java.util.ArrayList;
+
+public class Order {
+    private String userName;
+    private String userMobile;
+    private String userAddress;
+    private String orderId;
+
+
+
+    //size of cartBusinesses will be equal to size of states, i'th element of states will contain the state of i'th element of cartBusiness
+    private ArrayList<CartBusiness> cartBusinesses;
+    //State = 0 ->Not confirmed by Chaze
+    //State = 1 ->confirmed by Chaze
+    //State = 2 ->confirmed by Business
+    //State = 3 ->Order picked up
+    //State = 4 ->Delivered
+    private ArrayList<Integer> states;
+
+
+    public Order(String userName, String userMobile, String userAddress, String orderId, ArrayList<CartBusiness> cartBusinesses, ArrayList<Integer> states) {
+        this.userName = userName;
+        this.userMobile = userMobile;
+        this.userAddress = userAddress;
+        this.orderId = orderId;
+        this.cartBusinesses = cartBusinesses;
+        this.states = states;
+    }
+
+}
